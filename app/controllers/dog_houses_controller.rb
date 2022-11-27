52b638542b75e5652adee38f1d089b84861b0ee9
James Njenga <knjenga94@gmail.com>
2022-11-27 22:07:35 +0300

    Completed lab

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -2,11 +2,21 @@ class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    dog_house = DogHouse.find(params[:id])
+    dog_house =find_dog_by_id
     render json: dog_house, include: :reviews
   end
 
+  def destroy
+    dog=find_dog_by_id
+    dog.destroy
+  
+  end
+
   private
+
+  def find_dog_by_id
+    DogHouse.find(params[:id])
+  end
 
   def render_not_found_response
     render json: { error: "Dog house not found" }, status: :not_found
